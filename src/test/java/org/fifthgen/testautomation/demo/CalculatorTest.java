2a7748549bd3682c5d41a1ba8002b98961ea5864
Saminda Peramuna <saminda_lk@msn.com>
2024-03-30 15:57:18 +0400

    Initial commit

---
+++ src/test/java/org/fifthgen/testautomation/demo/CalculatorTest.java
@@ -0,0 +1,37 @@
+package org.fifthgen.testautomation.demo;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CalculatorTest {
+    Calculator calc;
+    @Before
+    public void setUp() {
+        calc = new Calculator();
+    }
+
+    @Test
+    public void addTwoNumbers() {
+        assertEquals(20, calc.add(10, 10));
+
+    }
+
+    @Test
+    public void addThreeNumbers() {
+        assertEquals(20, calc.add(5, 5, 10));
+
+    }
+
+    @Test
+    public void subtractTwoNumbers() {
+        assertEquals(0, calc.subtract(10, 10));
+
+    }
+
+    @Test
+    public void subtractThreeNumbers() {
+        assertEquals(0, calc.subtract(20, 10, 10));
+    }
+}
